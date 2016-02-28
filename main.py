40920ecd2b595be68c1d24dc2a4ca5b4037e6bec
Miha Černe <miha.cerne1@gmail.com>
2016-02-28 17:19:29 +0100

    added functions for matrix creations and chain creations

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
 __author__ = 'FAMILY'
+
+
+def create_node_matrix(width, height):
+    node_matrix = []
+    for i in range(height):
+        node_matrix.append([])
+        for j in range(width):
+            node_matrix[i].append(0)
+    return node_matrix
+
+
+def create_edge_matrix(width, height, tessellation):
+    edge_matrix = []
+    directions = 0
+
+    if tessellation == "triangular":
+        directions = 3
+    elif tessellation == "rectangular":
+        directions = 2
+
+    for i in range(height):
+        edge_matrix.append([])
+        for j in range(width):
+            edge_matrix[i].append([])
+            for k in range(directions):
+                edge_matrix[i][j].append([0])
+    return edge_matrix
+
+
+def create_chain(alphabet, length, shape):
+    chain = {'alphabet': alphabet,
+             'length': length,
+             'shape': shape}
+    return chain
+
+
