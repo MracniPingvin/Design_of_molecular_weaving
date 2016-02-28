a73a2e53211d03de2b396607f589e47d46c34f63
Miha Černe <miha.cerne1@gmail.com>
2016-02-28 18:23:17 +0100

    renamed Lattice_draw.py to LatticeDraw.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from functions.core_functions import *
-from classes.Lattice_draw import LatticeDraw
+from classes.LatticeDraw import LatticeDraw
 
 a = LatticeDraw(create_matrix([5, 5], 'triangular'), [500, 500], 'triangular')
 a.draw()
