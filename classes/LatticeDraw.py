3ebbbf61e0366dbe901ededbe72e1fe3775bec9d
Miha Černe <miha.cerne1@gmail.com>
2016-03-05 17:03:59 +0100

    adapted drawing functions for both triangular and rectangular lattices, added functions for drawing the lattice

--- classes/LatticeDraw.py
+++ classes/LatticeDraw.py
@@ -13,22 +13,28 @@ class LatticeDraw(object):
         self.node_width = len(matrix[0])
         self.node_height = len(matrix)
 
-        self.node_map = self.generate_node_map()  # the coordinates of each node
+        self.node_map = self.generate_node_map(0)
+        self.node_maps = [self.generate_node_map(-5), self.generate_node_map(0), self.generate_node_map(5)]   # the coordinates of each node
 
         self.line_thickness = 5  # thickness of the connection lines
         self.circle_radius = 10  # radius of the node circles
 
+        self.offset_list = [-self.line_thickness, 0, self.line_thickness]
+        self.text_offset_list = [-15, 0, 15]
+        self.color_list = ["#FF0000", "#00FF00", "#0000FF"]
+        self.text_space = 15
     def draw(self):
         master = Tk()
         self.canvas = Canvas(master, width=self.canvas_width, height=self.canvas_height)
         self.canvas.pack()
         self.draw_node_map()
+        self.segment_draw_triangular()
         mainloop()
 
     def create_circle(self, x, y, r, **kwargs):
         self.canvas.create_oval(x - r, y - r, x + r, y + r, **kwargs)
 
-    def generate_node_map(self):
+    def generate_node_map(self, offset):
         if self.tessellation == "triangular":
             distance = self.canvas_width // (self.node_width + self.node_height * 0.5 + 1)
         elif self.tessellation == "rectangular":
@@ -36,25 +42,90 @@ class LatticeDraw(object):
 
         node_map = []
         for i in range(self.node_height):
+            node_map.append([])
             for j in range(self.node_width):
                 if self.tessellation == "triangular":
-                    node_map.append([i * (distance // 2) + j * distance + distance,
-                                     i * (distance * (3 ** (1/2))/2) + distance])
+                    node_map[i].append([i * (distance // 2) + j * distance + distance + offset,
+                                        i * (distance * (3 ** (1/2))/2) + distance])
                 elif self.tessellation == "rectangular":
-                    node_map.append([distance + i * distance, distance + j * distance])
+                    node_map[i].append([distance + j * distance,distance + i * distance])
         return node_map
 
     def draw_node_map(self):
         count = 0
         for i in self.node_map:
-            count += 1
-            self.create_circle(i[0], i[1], self.circle_radius, fill="black")
-            self.canvas.create_text(i[0]+25, i[1]-20, text=count, fill="black", font=("Purisa", 15))
+            for j in i:
+                count += 1
+                self.create_circle(j[0], j[1], self.circle_radius, fill="black")
+                self.canvas.create_text(j[0]+25, j[1]-20, text=count, fill="black", font=("Purisa", 15))
 
-    # def segment_draw_triangular(self, start_node):
-    #     offset = self.line_thickness
-    #     soffset = int(offset * 0.70)
-    #     text_offset = 15
+    def segment_draw_rectangular(self):
+        distance = self.canvas_height // (self.node_height + 1)
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[1])):
+                for k in range(2):
+                    for l in range(len(self.matrix[i][j][k])):
+                        if k == 0:
+                            self.canvas.create_line(self.node_map[i][j][0] + self.offset_list[l],
+                                                    self.node_map[i][j][1] + self.offset_list[l],
+                                                    self.node_map[i][j][0] + self.offset_list[l],
+                                                    self.node_map[i][j][1] + distance + self.offset_list[l],
+                                                    fill=self.color_list[l], width=self.line_thickness)
+                            self.canvas.create_text(self.node_map[i][j][0] + self.text_space,
+                                                    (2*self.node_map[i][j][1] + distance) // 2 + self.text_offset_list[l],
+                                                    text=self.matrix[i][j][k][l],
+                                                    fill=self.color_list[l],
+                                                    font=("Purisa", 15))
+                        elif k == 1:
+                            self.canvas.create_line(self.node_map[i][j][0] + self.offset_list[l],
+                                                    self.node_map[i][j][1] + self.offset_list[l],
+                                                    self.node_map[i][j][0] + distance + self.offset_list[l],
+                                                    self.node_map[i][j][1] + self.offset_list[l],
+                                                    fill=self.color_list[l], width=self.line_thickness)
+                            self.canvas.create_text((2*self.node_map[i][j][0] + distance) // 2 + self.text_offset_list[l],
+                                                    self.node_map[i][j][1] + self.text_space,
+                                                    text=self.matrix[i][j][k][l],
+                                                    fill=self.color_list[l],
+                                                    font=("Purisa", 15))
+    def segment_draw_triangular(self):
+        distance = self.canvas_width // (self.node_width + self.node_height * 0.5 + 1)
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[1])):
+                for k in range(3):
+                    for l in range(len(self.matrix[i][j][k])):
+                        if k == 0:
+                            self.canvas.create_line(self.node_maps[l][i][j][0],
+                                                    self.node_maps[l][i][j][1],
+                                                    self.node_maps[l][i][j][0] - distance//2,
+                                                    self.node_maps[l][i][j][1] + distance * (3 ** (1/2))/2,
+                                                    fill=self.color_list[l], width=self.line_thickness)
+                            self.canvas.create_text((2*self.node_maps[l][i][j][0] - distance//2) // 2 - self.text_offset_list[l] - self.text_offset_list[2],
+                                                    (2*self.node_maps[l][i][j][1] + distance * (3 ** (1/2))/2) // 2 + self.text_offset_list[l],
+                                                    text=self.matrix[i][j][k][l],
+                                                    fill=self.color_list[l],
+                                                    font=("Purisa", 15))
+                        elif k == 1:
+                            self.canvas.create_line(self.node_maps[l][i][j][0],
+                                                    self.node_maps[l][i][j][1],
+                                                    self.node_maps[l][i][j][0] + distance//2,
+                                                    self.node_maps[l][i][j][1] + distance * (3 ** (1/2))/2,
+                                                    fill=self.color_list[l], width=self.line_thickness)
+                            self.canvas.create_text((2*self.node_maps[l][i][j][0] + distance//2) // 2 + self.text_offset_list[l]//3.8 - self.text_offset_list[2],
+                                                    (2*self.node_maps[l][i][j][1] + distance * (3 ** (1/2))/2) // 2 + self.text_offset_list[l],
+                                                    text=self.matrix[i][j][k][l],
+                                                    fill=self.color_list[l],
+                                                    font=("Purisa", 15))
+                        elif k == 2:
+                            self.canvas.create_line(self.node_maps[l][i][j][0],
+                                                    self.node_maps[l][i][j][1] + self.offset_list[l],
+                                                    self.node_maps[l][i][j][0] + distance,
+                                                    self.node_maps[l][i][j][1] + self.offset_list[l],
+                                                    fill=self.color_list[l], width=self.line_thickness)
+                            self.canvas.create_text((2*self.node_map[i][j][0] + distance) // 2 + self.text_offset_list[l],
+                                                    self.node_maps[l][i][j][1] + self.text_space,
+                                                    text=self.matrix[i][j][k][l],
+                                                    fill=self.color_list[l],
+                                                    font=("Purisa", 15))
     #     for i in self.matrix[start_node[1]][start_node[0]]:
     #         count = 0
     #         letter = i.lower()
@@ -212,4 +283,3 @@ class LatticeDraw(object):
     #                 self.canvas.create_text((self.node_map[start_node][0] + self.node_map[end_node][0]) // 2 + 15,
     #                                         self.node_map[start_node][1] + text_offset,
     #                                         text=letter, fill="#0000FF")
-    #
--- main.py
+++ main.py
@@ -6,19 +6,34 @@ create_global_matrix_references(["tile", "lattice"], [create_matrix([5, 5], "tri
 
 for i in globals.matrices["tile"]:
     print(i)
+# lay_chain("tile", create_chain("a", []), 1, 2, 0)
+# lay_chain("tile", create_chain("a", []), 1, 2, 90)
+# lay_chain("tile", create_chain("a", []), 1, 2, 180)
+# lay_chain("tile", create_chain("a", []), 1, 2, 270)
 
-lay_chain("tile", create_chain("ab", [0]), 0, 2, 0)
-lay_chain("tile", create_chain("ab", [60]), 0, 2, 0)
-lay_chain("tile", create_chain("ab", [120]), 0, 2, 0)
-lay_chain("tile", create_chain("ab", [180]), 0, 2, 0)
-lay_chain("tile", create_chain("ab", [240]), 0, 2, 0)
-lay_chain("tile", create_chain("ab", [300]), 0, 2, 0)
+lay_chain("tile", create_chain("a", []), 1, 2, 0)
+lay_chain("tile", create_chain("a", []), 1, 2, 60)
+lay_chain("tile", create_chain("a", []), 1, 2, 120)
+lay_chain("tile", create_chain("a", []), 1, 2, 180)
+lay_chain("tile", create_chain("a", []), 1, 2, 240)
+lay_chain("tile", create_chain("a", []), 1, 2, 300)
+lay_chain("tile", create_chain("a", []), 1, 2, 0)
+lay_chain("tile", create_chain("a", []), 1, 2, 60)
+lay_chain("tile", create_chain("a", []), 1, 2, 120)
+lay_chain("tile", create_chain("a", []), 1, 2, 180)
+lay_chain("tile", create_chain("a", []), 1, 2, 240)
+lay_chain("tile", create_chain("a", []), 1, 2, 300)
+lay_chain("tile", create_chain("a", []), 1, 2, 0)
+lay_chain("tile", create_chain("a", []), 1, 2, 60)
+lay_chain("tile", create_chain("a", []), 1, 2, 120)
+lay_chain("tile", create_chain("a", []), 1, 2, 180)
+lay_chain("tile", create_chain("a", []), 1, 2, 240)
+lay_chain("tile", create_chain("a", []), 1, 2, 300)
 for i in globals.matrices["tile"]:
     print(i)
 
-
-# a = LatticeDraw(create_matrix([5, 5], "rectangular"), [500, 500], "rectangular", ["abcd", "efgh"])
-# a.draw()
+a = LatticeDraw(globals.matrices["tile"], [1000, 700], "triangular", ["abcd", "efgh"])
+a.draw()
 
 
 
